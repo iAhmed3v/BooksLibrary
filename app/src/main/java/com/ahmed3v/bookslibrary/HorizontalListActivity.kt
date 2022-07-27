040fb9a67dc237a4c6fc60102cd4b1370d18974e
iAhmed3v <devahmdali@gmail.com>
2022-07-27 15:42:44 +0300

    update HorizontalListActivity.kt and activity_horizontal_list.xml

--- app/src/main/java/com/ahmed3v/bookslibrary/HorizontalListActivity.kt
+++ app/src/main/java/com/ahmed3v/bookslibrary/HorizontalListActivity.kt
@@ -2,10 +2,27 @@ package com.ahmed3v.bookslibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmed3v.bookslibrary.adapter.BookCardAdapter
+import com.ahmed3v.bookslibrary.const.Layout
+import com.ahmed3v.bookslibrary.databinding.ActivityHorizontalListBinding
 
 class HorizontalListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHorizontalListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_horizontal_list)
+        binding = ActivityHorizontalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.horizontalRecyclerView.adapter = BookCardAdapter(
+            applicationContext,
+            Layout.HORIZONTAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.horizontalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
