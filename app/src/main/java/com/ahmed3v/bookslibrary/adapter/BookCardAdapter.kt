9ef1e94d67e4c4c23a3cde07ed8037a5377c7413
iAhmed3v <devahmdali@gmail.com>
2022-07-27 14:14:58 +0300

    create Layout.kt file and update BookCardAdapter.kt

--- app/src/main/java/com/ahmed3v/bookslibrary/adapter/BookCardAdapter.kt
+++ app/src/main/java/com/ahmed3v/bookslibrary/adapter/BookCardAdapter.kt
@@ -9,21 +9,21 @@ import androidx.recyclerview.widget.RecyclerView
  * Adapter to inflate the appropriate list item layout and populate the view with information
  * from the appropriate data source
  */
-class FootballerCardAdapter(
+class BookCardAdapter(
     private val context: Context?,
     private val layout: Int
-): RecyclerView.Adapter<FootballerCardAdapter.FootballerCardViewHolder>() {
+): RecyclerView.Adapter<BookCardAdapter.BookCardViewHolder>() {
 
     // TODO: Initialize the data using the List found in data/DataSource
 
     /**
      * Initialize view elements
      */
-    class FootballerCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
+    class BookCardViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
         // TODO: Declare and initialize all of the list item UI components
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FootballerCardViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookCardViewHolder {
         // TODO: Use a conditional to determine the layout type and set it accordingly.
         //  if the layout variable is Layout.GRID the grid list item should be used. Otherwise the
         //  the vertical/horizontal list item should be used.
@@ -32,12 +32,12 @@ class FootballerCardAdapter(
 
         // TODO: Null should not be passed into the view holder. This should be updated to reflect
         //  the inflated layout.
-        return FootballerCardViewHolder(null)
+        return BookCardViewHolder(null)
     }
 
     override fun getItemCount(): Int = 0 // TODO: return the size of the data set instead of 0
 
-    override fun onBindViewHolder(holder: FootballerCardViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: BookCardViewHolder, position: Int) {
         // TODO: Get the data at the current position
         // TODO: Set the image resource for the current dog
         // TODO: Set the text for the current dog's name
