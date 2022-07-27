ab8b44d6b4aeb0ee06eef4bcc9efdd8b95629805
iAhmed3v <devahmdali@gmail.com>
2022-07-27 15:33:24 +0300

    create a Grid, Horizontal and Vertical lists activities

---
+++ app/src/main/java/com/ahmed3v/bookslibrary/VerticalListActivity.kt
@@ -0,0 +1,11 @@
+package com.ahmed3v.bookslibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerticalListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vertical_list)
+    }
+}
