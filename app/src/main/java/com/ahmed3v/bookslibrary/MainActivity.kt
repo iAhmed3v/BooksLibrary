ccf62ca16dc88c9008caaef8d32c1c1363e71568
iAhmed3v <devahmdali@gmail.com>
2022-07-27 16:11:42 +0300

    update MainActivity.kt and activity_main.xml

--- app/src/main/java/com/ahmed3v/bookslibrary/MainActivity.kt
+++ app/src/main/java/com/ahmed3v/bookslibrary/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.ahmed3v.bookslibrary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmed3v.bookslibrary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var listIntent: Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Setup view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Launch the VerticalListActivity on verticalBtn click
+        binding.verticalBtn.setOnClickListener { launchVertical() }
+
+        // Launch the HorizontalListActivity on horizontalBtn click
+        binding.horizontalBtn.setOnClickListener { launchHorizontal() }
+
+        // Launch the GridListActivity on gridBtn click
+        binding.gridBtn.setOnClickListener { launchGrid() }
+    }
+
+    private fun launchVertical() {
+        listIntent = Intent(this, VerticalListActivity::class.java)
+        startActivity(listIntent)
+    }
+
+    private fun launchHorizontal() {
+        listIntent = Intent(this, HorizontalListActivity::class.java)
+        startActivity(listIntent)
+    }
+
+    private fun launchGrid() {
+        listIntent = Intent(this, GridListActivity::class.java)
+        startActivity(listIntent)
     }
 }
