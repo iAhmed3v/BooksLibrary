89ae5f475e019c9249c3706bcfdb3c47aca53962
iAhmed3v <devahmdali@gmail.com>
2022-07-27 15:45:09 +0300

    update VerticalListActivity.kt and activity_vertical_list.xml

--- app/src/main/java/com/ahmed3v/bookslibrary/VerticalListActivity.kt
+++ app/src/main/java/com/ahmed3v/bookslibrary/VerticalListActivity.kt
@@ -2,10 +2,27 @@ package com.ahmed3v.bookslibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmed3v.bookslibrary.adapter.BookCardAdapter
+import com.ahmed3v.bookslibrary.const.Layout
+import com.ahmed3v.bookslibrary.databinding.ActivityVerticalListBinding
 
 class VerticalListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVerticalListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vertical_list)
+        binding = ActivityVerticalListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.verticalRecyclerView.adapter = BookCardAdapter(
+            applicationContext,
+            Layout.VERTICAL
+        )
+
+        // Specify fixed size to improve performance
+        binding.verticalRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
