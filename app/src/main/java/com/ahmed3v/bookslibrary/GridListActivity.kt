cdd962198f960f6f9817aed3a447180c5193a271
iAhmed3v <devahmdali@gmail.com>
2022-07-27 15:39:12 +0300

    update GridListActivity.kt and activity_grid_list.xml

--- app/src/main/java/com/ahmed3v/bookslibrary/GridListActivity.kt
+++ app/src/main/java/com/ahmed3v/bookslibrary/GridListActivity.kt
@@ -2,10 +2,27 @@ package com.ahmed3v.bookslibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmed3v.bookslibrary.adapter.BookCardAdapter
+import com.ahmed3v.bookslibrary.const.Layout
+import com.ahmed3v.bookslibrary.databinding.ActivityGridListBinding
 
 class GridListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGridListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_grid_list)
+        binding = ActivityGridListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gridRecyclerView.adapter = BookCardAdapter(
+            applicationContext,
+            Layout.GRID
+        )
+
+        // Specify fixed size to improve performance
+        binding.gridRecyclerView.setHasFixedSize(true)
+
+        // Enable up button for backward navigation
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
